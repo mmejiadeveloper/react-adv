92d54ea3bead1cc313f7437903e2ef5b0cfdef30
Miguel Mejia <miguel.mejia@flowhub.com>
2022-02-14 23:37:05 -0500

    componentes lazy agregados

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Navigate, NavLink, Route, Routes } from "react-router-do
 
 import logo from '../logo.svg'
 
+import {LazyPage1, LazyPage2, LazyPage3} from '../01-lazyload/pages'
+
 export const Navigation = () => {
   return (
     <BrowserRouter>
@@ -10,21 +12,21 @@ export const Navigation = () => {
           <img src={logo} alt="React Logo" />
           <ul>
             <li>
-              <NavLink to="/home" className={({ isActive }) => isActive ? 'nav-active' : ''} >Home</NavLink>
+              <NavLink to="/lazy1" className={({ isActive }) => isActive ? 'nav-active' : ''} >Lazy 1</NavLink>
             </li>
             <li>
-              <NavLink to="/about" className={({ isActive }) => isActive ? 'nav-active' : ''} >About</NavLink>
+              <NavLink to="/lazy2" className={({ isActive }) => isActive ? 'nav-active' : ''} >Lazy 2</NavLink>
             </li>
             <li>
-              <NavLink to="/users" className={({ isActive }) => isActive ? 'nav-active' : ''}>Users</NavLink>
+              <NavLink to="/lazy3" className={({ isActive }) => isActive ? 'nav-active' : ''}>Lazy 3</NavLink>
             </li>
           </ul>
         </nav>
         <Routes>
-          <Route path="about" element={<h1>about</h1>} />
-          <Route path="users" element={<h1>users</h1>} />
-          <Route path="/home" element={<h1>home</h1>} />
-          <Route path="/*" element={<Navigate to="home" replace />} />
+          <Route path="lazy1" element={<LazyPage1/>} />
+          <Route path="lazy2" element={<LazyPage2/>} />
+          <Route path="lazy3" element={<LazyPage3/>} />
+          <Route path="/*" element={<Navigate to="lazy1" replace />} />
         </Routes>
       </div>
     </BrowserRouter>
